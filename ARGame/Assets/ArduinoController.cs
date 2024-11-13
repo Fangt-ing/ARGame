b17aee5a83b41a422a3c059a755623a3f8ba2389
kunzhu <jufangting@gmail.com>
2024-11-13 17:46:29 +0100

    mirgrate

---
+++ ARGame/Assets/ArduinoController.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using System.Linq;
+
+public class ArduinoController : MonoBehaviour
+{
+    private SerialPort stream;
+    private static List<GameObject> allPixels = new List<GameObject>();
+    private static List<int> allPixelData = new List<int>();
+    private static List<int[]> colorGroups = new List<int[]>();
+
+
+
+
+    private readonly int pixelsWidth = 15;
+    private readonly int pixelsHeight = 14;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateColorGroups();
+
+        for (int i = 0; i < pixelsHeight; i++)
+        {
+            for (int j = 0; j < pixelsWidth; j++)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.name = "Pixel" + i + "," + j;
+                Vector3 vect = new Vector3(i, j, 0);
+                Quaternion quat = new Quaternion();
+                cube.transform.SetPositionAndRotation(vect, quat);
+                allPixels.Add(cube);
+            }
+
+        }
+
+        stream = new SerialPort("COM4", 115200);
+        stream.ReadTimeout = 50;
+        stream.Open();
+
+        InvokeRepeating("UpdatePressureMap", 3f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        try
+        {
+            var data = stream.ReadLine();
+            if (data.Length < 2)
+            {
+                //return?
+            }
+            else if (data[0] == '0' || data[0] == '1' || data[0] == '2' || data[0] == '3' || data[0] == '4' || data[0] == '5' || data[0] == '6' || data[0] == '7' || data[0] == '8' || data[0] == '9')
+            {
+                data = data.Remove(data.Length - 1);
+                Debug.Log(data);
+                List<string> values = data.Split(',').ToList<string>();
+                foreach (var a in values)
+                {
+
+                    allPixelData.Add(Int32.Parse(a));
+                }
+            }
+
+        }
+        catch (TimeoutException e)
+        {
+            //Debug.Log(e);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ColorTheCubes();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ClearTheData();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            WriteToArduino();
+        }
+
+
+    }
+
+    private void UpdatePressureMap()
+    {
+        ClearTheData();
+        WriteToArduino();
+        Invoke("ColorTheCubes", 3f);
+    }
+
+    public void ColorTheCubes()
+    {
+
+        Debug.Log("Received this many pixels: " + allPixels.Count);
+        for (int i = 0; i < allPixels.Count; i++)
+        {
+            var c = allPixelData[i];
+            if (c <= colorGroups[0][1]) { allPixels[i].GetComponent<Renderer>().material.color = new Color32(0, 47, 255, 255); }
+            else if (c >= colorGroups[1][0] && c <= colorGroups[1][1]) { allPixels[i].GetComponent<Renderer>().material.color = new Color32(0, 191, 255, 0); }
+            else if (c >= colorGroups[2][0] && c <= colorGroups[2][1]) { allPixels[i].GetComponent<Renderer>().material.color = new Color32(13, 255, 0, 255); }
+            else if (c >= colorGroups[3][0] && c <= colorGroups[3][1]) { allPixels[i].GetComponent<Renderer>().material.color = new Color32(255, 255, 0, 255); }
+            else if (c >= colorGroups[4][0]) { allPixels[i].GetComponent<Renderer>().material.color = new Color32(255, 0, 0, 255); }
+
+            allPixels[i].transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            allPixels[i].transform.localScale += new Vector3(0f, 0f, 0.17f * allPixelData[i]);
+        }
+    }
+
+    private void CreateColorGroups()
+    {
+        int[] group1 = new int[2];
+        group1[0] = 0;
+        group1[1] = 4;
+
+        int[] group2 = new int[2];
+        group2[0] = 5;
+        group2[1] = 9;
+
+        int[] group3 = new int[2];
+        group3[0] = 10;
+        group3[1] = 14;
+
+        int[] group4 = new int[2];
+        group4[0] = 15;
+        group4[1] = 19;
+
+        int[] group5 = new int[2];
+        group5[0] = 20;
+        group5[1] = 24;
+
+        colorGroups.Add(group1);
+        colorGroups.Add(group2);
+        colorGroups.Add(group3);
+        colorGroups.Add(group4);
+        colorGroups.Add(group5);
+    }
+
+    private void WriteToArduino()
+    {
+        stream.WriteLine("A");
+        stream.BaseStream.Flush();
+    }
+
+    private void ClearTheData()
+    {
+        allPixelData.Clear();
+    }
+
+}
---
+++ ARGame/Assets/ChangementController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/* Image1_Personnages
+ * Image2_Boardgame
+ * Image3_Croissant
+ * Image3_Paris
+ * Image4_Drakar
+ * Image4_NorthernLight
+ * Image5_ChampTulipe
+ * Image6_Beer
+ * Image7_China
+ * 
+ * */
+public class ChangementController : MonoBehaviour
+{
+    public GameObject Image1;
+    public GameObject Image2;
+    public Text txt1;
+    public Text txt2;
+    public Sprite Image1_Personnages;
+    public Sprite Image2_Boardgame;
+    public Sprite Image3_Croissant;
+    public Sprite Image3_Paris;
+    public Sprite Image4_Drakar;
+    public Sprite Image4_NorthernLight;
+    public Sprite Image5_ChampTulipe;
+    public Sprite Image6_Beer;
+    public Sprite Image7_China; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MyMethod());
+          
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator MyMethod()
+    {
+        // Parameter of the text ! 3
+        txt1.color = Color.white;
+        txt2.color = Color.white;
+        //txt1.fontSize = 18;
+        // Introduction of the game 
+
+
+        // First presentation of the character 
+        Image1.GetComponent<SpriteRenderer>().sprite = Image1_Personnages;
+        txt1.text = "It’s summer holiday for the grandson. Grandma is taking her grandson on a world trip. ";
+        txt2.text = "";
+        yield return new WaitForSeconds(2);
+
+        // Second presentation of the board game 
+        Image1.GetComponent<SpriteRenderer>().sprite = Image2_Boardgame;
+        txt1.text = "The path of this adventure is draw, follow it to discover the next...";
+        txt2.text = "";
+        yield return new WaitForSeconds(2);
+
+        // Third : Presentation France
+        Image1.GetComponent<SpriteRenderer>().sprite = Image3_Paris;
+        
+        txt1.text = "The journey starts from Paris in France.";
+        txt2.text = "They will visit Eiffel tower and eat croissant there.";
+        yield return new WaitForSeconds(2);
+        Image1.GetComponent<SpriteRenderer>().sprite = Image3_Croissant;
+        yield return new WaitForSeconds(2);
+
+        // Fourth : Presentation Norway
+        Image1.GetComponent<SpriteRenderer>().sprite = Image4_Drakar;       
+        txt1.text = "After Paris, both will head to Norway and check on the Vikings heritage. ";
+        txt2.text = "They will also enjoy the northern light during the stay in Norway.";
+        yield return new WaitForSeconds(2); 
+        Image1.GetComponent<SpriteRenderer>().sprite = Image4_NorthernLight;
+        yield return new WaitForSeconds(2);
+        // Fifth : Presentation Netherlands
+        Image1.GetComponent<SpriteRenderer>().sprite = Image5_ChampTulipe;
+        //Image2.SetActive(false);
+        txt1.text = "Fragrance of the tulips welcomes all travellers including grandma and grandson in Netherland. ";
+        txt2.text = "";
+        yield return new WaitForSeconds(2);
+
+        // Six : Presentation Germany
+        Image1.GetComponent<SpriteRenderer>().sprite = Image6_Beer;
+        txt1.text = "Last stop in Europe will be the industrial house of Europe, Germany, both are enjoying... ";
+        txt2.text = "Grandma takes also a bit German fine beer, but for grandson it’s too early now.";
+        yield return new WaitForSeconds(2);
+
+        // Seven : Presentation China
+        Image1.GetComponent<SpriteRenderer>().sprite = Image7_China;
+        txt1.text = "Flying in the sky from Europe to China, the Great Wall went into their eyes.";
+        txt2.text = "Landed on the land, a group of different style of architectures immersed the two visitors from far.";
+        yield return new WaitForSeconds(2);
+
+        // Eight : Let Enjoys
+
+
+    }
+}
